a517e932f82e5381fa63994fa8c4d11d9d6a8ffe
Francesco Pompo <deade1e@users.noreply.github.com>
2026-05-25 22:47:28 +0200

    simplify main

--- src/bin/rbw-agent/main.rs
+++ src/bin/rbw-agent/main.rs
@@ -10,7 +10,7 @@ mod ssh_agent;
 mod state;
 mod timeout;
 
-async fn tokio_main(startup_ack: Option<crate::daemon::StartupAck>) -> anyhow::Result<()> {
+async fn async_main(startup_ack: Option<crate::daemon::StartupAck>) -> anyhow::Result<()> {
     let listener = crate::sock::listen()?;
 
     if let Some(startup_ack) = startup_ack {
@@ -54,7 +54,7 @@ async fn tokio_main(startup_ack: Option<crate::daemon::StartupAck>) -> anyhow::R
     Ok(())
 }
 
-fn real_main() -> anyhow::Result<()> {
+fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let no_daemonize = std::env::args()
@@ -69,31 +69,9 @@ fn real_main() -> anyhow::Result<()> {
         log::warn!("{e}");
     }
 
-    let (w, r) = std::sync::mpsc::channel();
     // can't use tokio::main because we need to daemonize before starting the
     // tokio runloop, or else things break
-    // unwrap is fine here because there's no good reason that this should
-    // ever fail
-    tokio::runtime::Runtime::new().unwrap().block_on(async {
-        if let Err(e) = tokio_main(startup_ack).await {
-            // this unwrap is fine because it's the only real option here
-            w.send(e).unwrap();
-        }
-    });
-
-    if let Ok(e) = r.recv() {
-        return Err(e);
-    }
+    tokio::runtime::Runtime::new()?.block_on(async { async_main(startup_ack).await })?;
 
     Ok(())
-}
-
-fn main() {
-    let res = real_main();
-
-    if let Err(e) = res {
-        // XXX log file?
-        eprintln!("{e:#}");
-        std::process::exit(1);
-    }
 }
